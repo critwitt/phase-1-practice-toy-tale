41a5d8fdf0455a5c78ea822e370055e87e355586
Chris Witte <christopher.t.witte@gmail.com>
2022-08-16 11:56:20 -0400

    Completed practice code challenge

--- src/index.js
+++ src/index.js
@@ -12,4 +12,72 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys")
+  .then(res => res.json())
+  .then(data => data.forEach(renderToyCard))
+  .catch(err => console.log(err));
+
+  function renderToyCard(toyData) {
+    const toyCard = document.createElement("div");
+    const name = document.createElement("h2");
+    const image = document.createElement("img");
+    const likes = document.createElement("p");
+    const likeBtn = document.createElement("button");
+
+    toyCard.className = "card"
+    image.className = "toy-avatar";
+    likeBtn.className = "like-btn";
+
+    name.textContent = toyData.name;
+    image.src = toyData.image;
+    likes.textContent = `${toyData.likes} Likes`;
+    likeBtn.textContent = "Like ❤️";
+    likeBtn.id = toyData.id;
+
+    likeBtn.addEventListener("click", () => {
+      toyData.likes += 1;
+      likes.textContent = `${toyData.likes} Likes`;
+      fetch(`http://localhost:3000/toys/${toyData.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({
+          likes: toyData.likes
+        })
+      })
+    })
+
+    toyCard.append(name, image, likes, likeBtn);
+    document.querySelector("#toy-collection").append(toyCard);
+
+  }
+
+  const form = document.querySelector(".add-toy-form");
+  form.addEventListener("submit", handleForm);
+
+  function handleForm(e) {
+
+    e.preventDefault();
+
+    const newToy = {
+      name: e.target.name.value,
+      image: e.target.image.value,
+      likes: 0
+    };
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(newToy)
+    })
+
+    renderToyCard(newToy);
+  }
+
 });
